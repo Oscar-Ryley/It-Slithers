2901d00ddb5364f5f02323704ed7703234d8eea2
Oscar Ryley <oscar.ryley@gmail.com>
2024-04-07 14:19:43 +0100

    Implemented Lose and Win Screens
    Death Screen activates if you spend too long watching the videos and therefore attract the monster in the Woods.

---
+++ Godot/Global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var watched = 0
---
+++ Godot/Lose Text.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+func _ready():
+	visible = false
+
+func _on_win_area_3d_body_entered(body):
+	if Global.watched >= 5:
+		visible = true
