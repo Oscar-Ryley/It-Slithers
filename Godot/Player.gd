8e54c6a1cf3d80b43ff4fc1c65cccab3e472a6c4
Oscar Ryley <oscar.ryley@gmail.com>
2024-04-07 13:28:01 +0100

    Implemented music and chanting
    Using 2D Audio container for the backing track and 3D Spatial audio container for chanting sounds.

--- Godot/Player.gd
+++ Godot/Player.gd
@@ -17,8 +17,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.005)
-			camera.rotate_x(-event.relative.y * 0.005)
+			neck.rotate_y(-event.relative.x * 0.002)
+			camera.rotate_x(-event.relative.y * 0.002)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
 
